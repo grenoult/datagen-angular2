ee7933079b7dbd53d32b2570e691d7604b84ff95
grenoult <guillaume.renoult@gmail.com>
2017-05-08 19:56:14 +1000

    Fix gitignore

---
+++ src/app/mainform.component.js
@@ -0,0 +1,22 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var MainformComponent = (function () {
+    function MainformComponent() {
+    }
+    return MainformComponent;
+}());
+MainformComponent = __decorate([
+    core_1.Component({
+        selector: 'main-form',
+        template: "\n        <h1>Hey, this is the main form yay</h1>\n    "
+    })
+], MainformComponent);
+exports.MainformComponent = MainformComponent;
+//# sourceMappingURL=mainform.component.js.map
---
+++ src/app/mainform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-form',
+    template: `
+        <h1>Hey, this is the main form yay</h1>
+    `
+})
+
+export class MainformComponent  {
+}
